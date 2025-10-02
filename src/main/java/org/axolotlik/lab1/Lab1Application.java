97999f82d0786d885819ae3f77f6d8a9e3576e47
axolotlik <slsanya2006@gmail.com>
2025-10-02 18:20:28 +0300

    Lab 1 v1 modified (fixed output order)

--- src/main/java/org/axolotlik/lab1/Lab1Application.java
+++ src/main/java/org/axolotlik/lab1/Lab1Application.java
@@ -3,9 +3,11 @@ package org.axolotlik.lab1;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.annotation.Order;
 
 
 @SpringBootApplication
+@Order(2)
 public class Lab1Application implements CommandLineRunner {
 
     public static void main(String[] args) {
--- src/main/java/org/axolotlik/lab1/Second.java
+++ src/main/java/org/axolotlik/lab1/Second.java
@@ -5,7 +5,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
-@Order(2)  // виконається після First
+@Order(3)  // виконається після main
 public class Second implements CommandLineRunner {
     @Override
     public void run(String... args) {
