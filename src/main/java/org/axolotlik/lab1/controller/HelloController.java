eb3f22aecffddf47a6fd4cc4a6d6ba3d4fe98b64
axolotlik <slsanya2006@gmail.com>
2025-10-02 16:08:14 +0300

    Lab 1 v2

---
+++ src/main/java/org/axolotlik/lab1/controller/HelloController.java
@@ -0,0 +1,15 @@
+package org.axolotlik.lab1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello, World!";
+    }
+}
